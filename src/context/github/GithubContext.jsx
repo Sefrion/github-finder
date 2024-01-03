16451cd404806aca7d4a1dfd72fa6a088fae4509
Sefrion <ilyenkov96@gmail.com>
2024-01-03 11:44:52 +0200

    Search form added

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -15,20 +15,24 @@ export const GithubProvider = ({ children }) => {
 
 	const [state, dispatch] = useReducer(githubReducer, initialState);
 
-	// Get initial users (testing purposes)
-	const fetchUsers = async () => {
+	// Get search results
+	const searchUsers = async (text) => {
 		setLoading();
 
-		const res = await fetch(`${GITHUB_URL}/users`, {
+		const params = new URLSearchParams({
+			q: text,
+		});
+
+		const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
 			headers: {
 				Authorization: `token ${GITHUB_TOKEN}`,
 			},
 		});
-		const data = await res.json();
+		const { items } = await res.json();
 
 		dispatch({
 			type: 'GET_USERS',
-			payload: data,
+			payload: items,
 		});
 	};
 
@@ -43,7 +47,7 @@ export const GithubProvider = ({ children }) => {
 			value={{
 				users: state.users,
 				loading: state.loading,
-				fetchUsers,
+				searchUsers,
 			}}
 		>
 			{children}
@@ -56,3 +60,20 @@ GithubProvider.propTypes = {
 };
 
 export default GithubContext;
+
+// // Get initial users (testing purposes)
+// const fetchUsers = async () => {
+// 	setLoading();
+
+// 	const res = await fetch(`${GITHUB_URL}/users`, {
+// 		headers: {
+// 			Authorization: `token ${GITHUB_TOKEN}`,
+// 		},
+// 	});
+// 	const data = await res.json();
+
+// 	dispatch({
+// 		type: 'GET_USERS',
+// 		payload: data,
+// 	});
+// };
